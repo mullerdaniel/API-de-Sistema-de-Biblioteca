8c67c1cb49da16aaf361c2c75b945cf93013bb74
Daniel Müller <miladaniel370@gmail.com>
2026-02-19 16:25:08 -0300

    Feat: @PostMapping salvarUsuario

---
+++ biblioteca/src/main/java/com/example/biblioteca/controller/UsuarioController.java
@@ -0,0 +1,45 @@
+package com.example.biblioteca.controller;
+
+import com.example.biblioteca.model.Livro;
+import com.example.biblioteca.model.Usuario;
+import com.example.biblioteca.service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+
+    // METODO POST
+    @PostMapping
+    public Usuario salvarUsuario(
+            @RequestBody Usuario usuario
+    ){
+        try {
+            return usuarioService.salvarUsuario(usuario);
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
