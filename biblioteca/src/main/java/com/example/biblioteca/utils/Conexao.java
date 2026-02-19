8405c5d4f7229c85de43fc07e18f9c413de60083
Daniel Müller <miladaniel370@gmail.com>
2026-02-19 14:08:48 -0300

    Feat: Conexão com banco de dados

--- biblioteca/src/main/java/com/example/biblioteca/utils/Conexao.java
+++ biblioteca/src/main/java/com/example/biblioteca/utils/Conexao.java
@@ -1,5 +1,16 @@
 package com.example.biblioteca.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
 
+    private final static String URL = "";
+    private final static String USER = "root";
+    private final static String SENHA = "";
+
+    public Connection Conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, SENHA);
+    }
 }
