2cf142005e0a672ff9c51aa38c039d8016ef3d54
Daniel Müller <miladaniel370@gmail.com>
2026-02-19 14:36:52 -0300

    Feat: Adicionando metodo buscarLivro

--- biblioteca/src/main/java/com/example/biblioteca/dao/LivroDAO.java
+++ biblioteca/src/main/java/com/example/biblioteca/dao/LivroDAO.java
@@ -5,6 +5,8 @@ import com.example.biblioteca.utils.Conexao;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Repository
@@ -27,12 +29,43 @@ public class LivroDAO {
 
             if(rs.next()) {
                 livro.setId(rs.getInt(1));
+                return livro;
             }
         }
         return livro;
     }
 
     // BUSCARTODOS
+    public List<Livro> buscarLivro() throws SQLException {
+        List<Livro> listarLivros = new ArrayList<>();
+        String query = """
+                SELECT
+                id,
+                titulo,
+                autor,
+                ano_publicacao
+                FROM
+                livro
+                """;
+
+        try(Connection conn = Conexao.Conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+
+                listarLivros.add(new Livro(
+                    rs.getInt("id"),
+                    rs.getString("titulo"),
+                    rs.getString("autor"),
+                    rs.getInt("ano_publicacao")
+                        ));
+            }
+        }
+        return listarLivros;
+    }
+
     // BUSCARPORID
     // ATUALIZAR
     // DELETAR
