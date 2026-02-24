621835f3233b57486657a1b69e9f06a55fda27d6
Daniel Müller <miladaniel370@gmail.com>
2026-02-24 19:44:34 -0300

    Feat: Conclusao da classe UsuarioMapper

--- biblioteca/src/main/java/com/example/biblioteca/mapper/UsuarioMapper.java
+++ biblioteca/src/main/java/com/example/biblioteca/mapper/UsuarioMapper.java
@@ -1,4 +1,26 @@
 package com.example.biblioteca.mapper;
 
+import com.example.biblioteca.Dto.UsuarioRequisicaoDto;
+import com.example.biblioteca.Dto.UsuarioRespostaDto;
+import com.example.biblioteca.model.Usuario;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsuarioMapper {
+    public Usuario paraEntidade(
+            UsuarioRequisicaoDto usuarioRequisicaoDto) {
+        return new Usuario(
+                usuarioRequisicaoDto.nome(),
+                usuarioRequisicaoDto.email()
+        );
+    }
+    public UsuarioRespostaDto paraRespostaDto(
+            Usuario usuario
+    ){
+        return new UsuarioRespostaDto(
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getEmail()
+        );
+    }
 }
