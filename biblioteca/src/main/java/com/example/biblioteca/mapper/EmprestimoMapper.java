c76da42fd750f2e47a601a19dc8bb0657db1bf8f
Daniel Müller <miladaniel370@gmail.com>
2026-02-24 19:28:18 -0300

    Feat: Conclusao da classe EmprestimoMapper

--- biblioteca/src/main/java/com/example/biblioteca/mapper/EmprestimoMapper.java
+++ biblioteca/src/main/java/com/example/biblioteca/mapper/EmprestimoMapper.java
@@ -1,4 +1,30 @@
 package com.example.biblioteca.mapper;
 
+import com.example.biblioteca.Dto.EmprestimoRequisicaoDto;
+import com.example.biblioteca.Dto.EmprestimoRespostaDto;
+import com.example.biblioteca.model.Emprestimo;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmprestimoMapper {
+    public Emprestimo paraEntidade(
+            EmprestimoRequisicaoDto emprestimoRequisicaoDto){
+        return new Emprestimo(
+                emprestimoRequisicaoDto.livro_id(),
+                emprestimoRequisicaoDto.usuario_id(),
+                emprestimoRequisicaoDto.data_emprestimo(),
+                emprestimoRequisicaoDto.data_devolucao()
+        );
+    }
+    public EmprestimoRespostaDto paraRespostaDto(
+            Emprestimo emprestimo
+    ){
+        return new EmprestimoRespostaDto(
+                emprestimo.getId(),
+                emprestimo.getLivro_id(),
+                emprestimo.getUsuario_id(),
+                emprestimo.getData_emprestimo(),
+                emprestimo.getData_devolucao()
+        );
+    }
 }
