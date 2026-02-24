48ec18a877ab6e95b7b5f0646995fa154d422876
Daniel Müller <miladaniel370@gmail.com>
2026-02-24 19:42:15 -0300

    Feat: Conclusao da classe LivroMapper

--- biblioteca/src/main/java/com/example/biblioteca/mapper/LivroMapper.java
+++ biblioteca/src/main/java/com/example/biblioteca/mapper/LivroMapper.java
@@ -1,4 +1,29 @@
 package com.example.biblioteca.mapper;
 
+import com.example.biblioteca.Dto.LivroRequisicaoDto;
+import com.example.biblioteca.Dto.LivroRespostaDto;
+import com.example.biblioteca.model.Livro;
+import org.springframework.stereotype.Component;
+
+@Component
 public class LivroMapper {
+    public Livro paraEntidade(
+            LivroRequisicaoDto livroRequisicaoDto) {
+        return new Livro(
+                livroRequisicaoDto.titulo(),
+                livroRequisicaoDto.autor(),
+                livroRequisicaoDto.ano_publicacao()
+        );
+    }
+    public LivroRespostaDto paraRespostaDto(
+            Livro livro
+    ){
+        return new LivroRespostaDto(
+                livro.getId(),
+                livro.getTitulo(),
+                livro.getAutor(),
+                livro.getAno_publicacao()
+        );
+    }
+
 }
